ad2879a86628cb81c29cd4661cb550aa9ec1365d
Juan S Carrillo <juanscarrillo23@gmail.com>
2015-12-22 19:39:34 -0800

    Added a struct for keeping track of time and angles.

--- disco-watchface/src/disco-watchface.c
+++ disco-watchface/src/disco-watchface.c
@@ -1,7 +1,20 @@
 #include <pebble.h>
 
+#define DEGREES_IN_A_MINUTE 6 // 360 / 60
+#define DEGREES_IN_AN_HOUR 30 // 360 / 24
+
+typedef struct TimeInfo{
+  int hours;
+  int minutes;
+  int min_angle;
+  int hour_angle;
+  bool is_am;
+} TimeInfo;
+
+// Our static variables
 static Window * s_main_window;
 
+// Images
 static BitmapLayer *s_bitmap_layer_body;
 static BitmapLayer *s_bitmap_layer_h_arm;
 static BitmapLayer *s_bitmap_layer_m_arm;
@@ -9,6 +22,8 @@ static GBitmap * s_dancer_body;
 static GBitmap * s_minute_arm;
 static GBitmap * s_hour_arm;
 
+// Information about current time.
+static TimeInfo current_time;
 
 // Note: We forward declare all our functions here.
 // This way we can organize our functions in the code
@@ -63,8 +78,12 @@ static void main_window_load(Window * window) {
   Layer * window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
   GRect m_arm_bounds = GRect(55,20,120,120);
-  GRect h_arm_bounds = GRect(57,60,120,120);
+  GRect h_arm_bounds = GRect(40,27,120,120);
 
+
+  // We have three different layers: minute arm, the body, and the hour arm.
+  // Each must be in its own bitmap layer, so we must create three different
+  // bitmap layers.
   s_bitmap_layer_m_arm = create_new_bitmap_layer(m_arm_bounds, s_minute_arm, RESOURCE_ID_MINUTE_ARM);
   layer_add_child(window_layer, bitmap_layer_get_layer(s_bitmap_layer_m_arm));
 
@@ -76,7 +95,8 @@ static void main_window_load(Window * window) {
 
 }
 
-
+// This adds a new bitmap layer. Since we do this three times for
+// this app, this function prevents repetitive code.
 static BitmapLayer * create_new_bitmap_layer(GRect bounds,
                                              GBitmap * bitmap,
                                              uint32_t resource_id) {
@@ -87,6 +107,7 @@ static BitmapLayer * create_new_bitmap_layer(GRect bounds,
   return bitmap_layer;
 }
 
+// Destroy the bitmap layers once the app closes.
 static void main_window_unload(Window * window) {
   // Destroy images
   bitmap_layer_destroy(s_bitmap_layer_m_arm);
@@ -102,14 +123,28 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
   update_time();
 }
 
+// This function should only be called once when the app is started.
+static void initialize_time() {
+  time_t temp = time(NULL);
+  struct tm * tick_time = localtime(&temp);
+
+  current_time.hours = tick_time->tm_hour;
+  current_time.minutes = tick_time->tm_min;
+  current_time.is_am = tick_time->tm_hour < 12;
+  current_time.min_angle = tick_time->tm_hour * 360 / 60;
+  current_time.hour_angle = tick_time->tm_hour * 360 / 24;
+
+}
+
 static void update_time() {
   // We get the time structure from the update.
   time_t temp = time(NULL);
   struct tm * tick_time = localtime(&temp);
 
-  // Write the current hours and minutes into a buffer
-  // We use static to make the s_buffer persist accross
-  // calls.
-  static char s_buffer[12];
-  strftime(s_buffer, sizeof(s_buffer), "%r", tick_time);
+  current_time.hours = tick_time->tm_hour;
+  current_time.minutes = tick_time->tm_min;
+  current_time.is_am = tick_time->tm_hour < 12;
+  current_time.min_angle = (current_time.min_angle + DEGREES_IN_A_MINUTE) % 360;
+  current_time.hour_angle = (current_time.hour_angle + DEGREES_IN_AN_HOUR) % 360;
+
 }
